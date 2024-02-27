a5f7267b55fe1c518392842eeb490106d2d450ea
CherpakAndrii <a.cherpak-2026@kpi.ua>
2024-02-27 20:37:46 +0200

    Project divided into files; CRUD done

---
+++ store/models.py
@@ -0,0 +1,98 @@
+from sqlalchemy import (
+    create_engine,
+    MetaData,
+    Table,
+    Column,
+    Integer,
+    String,
+    Float,
+    DateTime,
+)
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+from pydantic import BaseModel, field_validator
+from config import (
+    DATABASE_URL,
+)
+
+# SQLAlchemy setup
+engine = create_engine(DATABASE_URL)
+metadata = MetaData()
+# Define the ProcessedAgentData table
+processed_agent_data = Table(
+    "processed_agent_data",
+    metadata,
+    Column("id", Integer, primary_key=True, index=True, autoincrement=True),
+    Column("road_state", String),
+    Column("user_id", Integer),
+    Column("x", Float),
+    Column("y", Float),
+    Column("z", Float),
+    Column("latitude", Float),
+    Column("longitude", Float),
+    Column("timestamp", DateTime),
+)
+SessionLocal = sessionmaker(bind=engine)
+session = SessionLocal()
+
+
+# SQLAlchemy model
+class ProcessedAgentDataInDB(BaseModel):
+    id: int
+    road_state: str
+    user_id: int
+    x: float
+    y: float
+    z: float
+    latitude: float
+    longitude: float
+    timestamp: datetime
+
+    def __init__(self, road_state: str, user_id: int, x: float, y: float, z: float, latitude: float, longitude: float, timestamp: datetime, id: int|None = None):
+        self.road_state = road_state
+        self.user_id = user_id
+        self.x = x
+        self.y = y
+        self.z = z
+        self.latitude = latitude
+        self.longitude = longitude
+        self.timestamp = timestamp
+        if id is not None:
+            self.id = id
+
+
+
+# FastAPI models
+class AccelerometerData(BaseModel):
+    x: float
+    y: float
+    z: float
+
+
+class GpsData(BaseModel):
+    latitude: float
+    longitude: float
+
+
+class AgentData(BaseModel):
+    user_id: int
+    accelerometer: AccelerometerData
+    gps: GpsData
+    timestamp: datetime
+
+    @classmethod
+    @field_validator("timestamp", mode="before")
+    def check_timestamp(cls, value):
+        if isinstance(value, datetime):
+            return value
+        try:
+            return datetime.fromisoformat(value)
+        except (TypeError, ValueError):
+            raise ValueError(
+                "Invalid timestamp format. Expected ISO 8601 format (YYYY-MM-DDTHH:MM:SSZ)."
+            )
+
+
+class ProcessedAgentData(BaseModel):
+    road_state: str
+    agent_data: AgentData
---
+++ store/processed_agent_data_routes.py
@@ -0,0 +1,62 @@
+from typing import List
+
+from fastapi import APIRouter
+
+from models import ProcessedAgentDataInDB, ProcessedAgentData, session
+
+processed_agent_data_router = APIRouter()
+
+
+@processed_agent_data_router.post("/")
+async def create_processed_agent_data(data: List[ProcessedAgentData]):
+    session.bulk_insert_objects(
+        [ProcessedAgentDataInDB(
+            a_data.road_state,
+            a_data.agent_data.user_id,
+            a_data.agent_data.accelerometer.x,
+            a_data.agent_data.accelerometer.y,
+            a_data.agent_data.accelerometer.z,
+            a_data.agent_data.gps.latitude,
+            a_data.agent_data.gps.longitude,
+            a_data.agent_data.timestamp
+        ) for a_data in data]
+    )
+    session.commit()
+    return
+
+
+@processed_agent_data_router.get("/{processed_agent_data_id}", response_model=ProcessedAgentDataInDB)
+def read_processed_agent_data(processed_agent_data_id: int):
+    return session.query(ProcessedAgentDataInDB).get(processed_agent_data_id)
+
+
+@processed_agent_data_router.get("/", response_model=list[ProcessedAgentDataInDB])
+def list_processed_agent_data():
+    return list(session.query(ProcessedAgentDataInDB).all())
+
+
+@processed_agent_data_router.put("/{processed_agent_data_id}", response_model=ProcessedAgentDataInDB)
+def update_processed_agent_data(processed_agent_data_id: int, data: ProcessedAgentData):
+    updated_instance = ProcessedAgentDataInDB(
+            data.road_state,
+            data.agent_data.user_id,
+            data.agent_data.accelerometer.x,
+            data.agent_data.accelerometer.y,
+            data.agent_data.accelerometer.z,
+            data.agent_data.gps.latitude,
+            data.agent_data.gps.longitude,
+            data.agent_data.timestamp,
+            id=processed_agent_data_id
+        )
+
+    session.merge(updated_instance)
+    session.commit()
+    return updated_instance
+
+
+@processed_agent_data_router.delete("/{processed_agent_data_id}", response_model=ProcessedAgentDataInDB)
+def delete_processed_agent_data(processed_agent_data_id: int):
+    obj = session.query(ProcessedAgentDataInDB).get(processed_agent_data_id)
+    session.delete(obj)
+    session.commit()
+    return obj

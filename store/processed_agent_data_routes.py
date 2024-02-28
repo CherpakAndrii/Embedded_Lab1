315a38bcff296d211ce76068d40ba4e45c5e0a55
CherpakAndrii <a.cherpak-2026@kpi.ua>
2024-02-28 22:19:57 +0200

    Table definition simplified and moved to separate file

--- store/processed_agent_data_routes.py
+++ store/processed_agent_data_routes.py
@@ -2,50 +2,51 @@ from typing import List
 
 from fastapi import APIRouter
 
-from models import ProcessedAgentDataInDB, ProcessedAgentData, session
+from models import ProcessedAgentData
+from database import ProcessedAgentDataInDB, session
 
 processed_agent_data_router = APIRouter()
 
 
 @processed_agent_data_router.post("/")
 async def create_processed_agent_data(data: List[ProcessedAgentData]):
-    session.bulk_insert_objects(
+    session.bulk_save_objects(
         [ProcessedAgentDataInDB(
-            a_data.road_state,
-            a_data.agent_data.user_id,
-            a_data.agent_data.accelerometer.x,
-            a_data.agent_data.accelerometer.y,
-            a_data.agent_data.accelerometer.z,
-            a_data.agent_data.gps.latitude,
-            a_data.agent_data.gps.longitude,
-            a_data.agent_data.timestamp
+            road_state=a_data.road_state,
+            user_id=a_data.agent_data.user_id,
+            x=a_data.agent_data.accelerometer.x,
+            y=a_data.agent_data.accelerometer.y,
+            z=a_data.agent_data.accelerometer.z,
+            latitude=a_data.agent_data.gps.latitude,
+            longitude=a_data.agent_data.gps.longitude,
+            timestamp=a_data.agent_data.timestamp
         ) for a_data in data]
     )
     session.commit()
     return
 
 
-@processed_agent_data_router.get("/{processed_agent_data_id}", response_model=ProcessedAgentDataInDB)
+@processed_agent_data_router.get("/{processed_agent_data_id}")
 def read_processed_agent_data(processed_agent_data_id: int):
     return session.query(ProcessedAgentDataInDB).get(processed_agent_data_id)
 
 
-@processed_agent_data_router.get("/", response_model=list[ProcessedAgentDataInDB])
+@processed_agent_data_router.get("/")
 def list_processed_agent_data():
     return list(session.query(ProcessedAgentDataInDB).all())
 
 
-@processed_agent_data_router.put("/{processed_agent_data_id}", response_model=ProcessedAgentDataInDB)
+@processed_agent_data_router.put("/{processed_agent_data_id}")
 def update_processed_agent_data(processed_agent_data_id: int, data: ProcessedAgentData):
     updated_instance = ProcessedAgentDataInDB(
-            data.road_state,
-            data.agent_data.user_id,
-            data.agent_data.accelerometer.x,
-            data.agent_data.accelerometer.y,
-            data.agent_data.accelerometer.z,
-            data.agent_data.gps.latitude,
-            data.agent_data.gps.longitude,
-            data.agent_data.timestamp,
+            road_state=data.road_state,
+            user_id=data.agent_data.user_id,
+            x=data.agent_data.accelerometer.x,
+            y=data.agent_data.accelerometer.y,
+            z=data.agent_data.accelerometer.z,
+            latitude=data.agent_data.gps.latitude,
+            longitude=data.agent_data.gps.longitude,
+            timestamp=data.agent_data.timestamp,
             id=processed_agent_data_id
         )
 
@@ -54,7 +55,7 @@ def update_processed_agent_data(processed_agent_data_id: int, data: ProcessedAge
     return updated_instance
 
 
-@processed_agent_data_router.delete("/{processed_agent_data_id}", response_model=ProcessedAgentDataInDB)
+@processed_agent_data_router.delete("/{processed_agent_data_id}")
 def delete_processed_agent_data(processed_agent_data_id: int):
     obj = session.query(ProcessedAgentDataInDB).get(processed_agent_data_id)
     session.delete(obj)
